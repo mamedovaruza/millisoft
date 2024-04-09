e35f1597638f44ded4f3c977a4989e08be60b0cd
Aruza <88088294+mamedovaruza@users.noreply.github.com>
2024-04-09 13:48:23 +0400

    .

--- js/js4/script.js
+++ js/js4/script.js
@@ -76,3 +76,5 @@
 // }
 
 // examResults(infos)
+
+
--- js/jsdom2/task8/script.js
+++ js/jsdom2/task8/script.js
@@ -1,3 +1,37 @@
 function Modal(params) {
 	console.log("ok");
 }
+
+// Sample array
+// var quotes = [
+// 	{
+// 		quote: "salam",
+// 		person: "aytac"
+// 	},
+// 	{
+// 		quote: "sagol",
+// 		person: "rena"
+// 	},
+// 	{
+// 		quote: "necesiz",
+// 		person: "aruza"
+// 	},
+// 	{
+// 		quote: "yaxsi",
+// 		person: "aysen"
+// 	}
+// ];
+
+
+// function Change() {
+// 	var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+// 	document.getElementById("container-quote").innerHTML =
+// 		`
+// 		<p>"${randomQuote.quote}"</p>
+// 		<h2>${randomQuote.person}</h2>
+// 	`
+
+// 	quotes.splice(quotes.indexOf(randomQuote), 1)
+// 	console.log(quotes);
+// }
